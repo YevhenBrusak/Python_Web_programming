094231dc0b9c21334f425dde16cd86fb41b3a496
Yevhen Brusak <brusak02@gmail.com>
2022-10-17 16:40:04 +0300

    Added cookies

--- Lab_2/Ex1_CGI/cgi-bin/form.py
+++ Lab_2/Ex1_CGI/cgi-bin/form.py
@@ -1,4 +1,6 @@
 import cgi
+from http import cookies
+import os
 
 form = cgi.FieldStorage()
 
@@ -20,6 +22,18 @@ if form.getvalue("fav_player"):
     fav_player = form.getvalue("fav_player")
 else : fav_player = "немає"
 
+cs = cookies.SimpleCookie(os.environ["HTTP_COOKIE"])
+counter = cs.get("counter")
+
+if counter is None:
+    print("Set-cookie: counter=0")
+else:
+    counterVal = int(counter.value)
+    counterVal += 1
+    print("Set-cookie: counter={}".format(counterVal))
+
+
+
 print("Content-type: text/html\n")
 print("""<!DOCTYPE HTML>
         <html>
@@ -34,5 +48,6 @@ print("<p>Перший текст: {}</p>".format(text1))
 print("<p>Другий текст: {}</p>".format(text2))
 print("<p>Улюблені клуби: {} {} {} {}</p>".format(realmadrid,barcelona,liverpool,psg))
 print("<p>Улюблений футболіст: {}</p>".format(fav_player))
+print("<p>Cookie: Кількість заповнених форм: {} </p> ".format(counterVal))
 print("""</body>
         </html>""")
